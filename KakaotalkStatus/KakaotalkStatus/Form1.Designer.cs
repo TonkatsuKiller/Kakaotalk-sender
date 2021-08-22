d7148b339e003c9baadc9128f7523330d9e78abe
12azm <89334326+12azm@users.noreply.github.com>
2021-08-22 16:21:59 +0900

    Add files via upload

---
+++ KakaotalkStatus/KakaotalkStatus/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace KakaotalkStatus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle13 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle14 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.List = new System.Windows.Forms.DataGridView();
+            this.Room = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valuec = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.startc = new System.Windows.Forms.Button();
+            this.Refresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.List)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // List
+            // 
+            this.List.AllowUserToAddRows = false;
+            this.List.AllowUserToDeleteRows = false;
+            this.List.AllowUserToResizeColumns = false;
+            this.List.AllowUserToResizeRows = false;
+            this.List.BackgroundColor = System.Drawing.Color.White;
+            this.List.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle13.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle13.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle13.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle13.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle13.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle13.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle13.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.List.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle13;
+            this.List.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.List.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Room,
+            this.Status});
+            dataGridViewCellStyle14.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle14.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle14.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle14.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle14.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle14.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle14.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.List.DefaultCellStyle = dataGridViewCellStyle14;
+            this.List.EnableHeadersVisualStyles = false;
+            this.List.Location = new System.Drawing.Point(12, 38);
+            this.List.MultiSelect = false;
+            this.List.Name = "List";
+            this.List.ReadOnly = true;
+            this.List.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.List.RowHeadersVisible = false;
+            this.List.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.List.RowTemplate.Height = 23;
+            this.List.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.List.Size = new System.Drawing.Size(258, 317);
+            this.List.TabIndex = 0;
+            // 
+            // Room
+            // 
+            this.Room.Frozen = true;
+            this.Room.HeaderText = "Room";
+            this.Room.Name = "Room";
+            this.Room.ReadOnly = true;
+            this.Room.Width = 170;
+            // 
+            // Status
+            // 
+            this.Status.Frozen = true;
+            this.Status.HeaderText = "Status";
+            this.Status.Name = "Status";
+            this.Status.ReadOnly = true;
+            this.Status.Width = 88;
+            // 
+            // valuec
+            // 
+            this.valuec.AutoSize = true;
+            this.valuec.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.valuec.Location = new System.Drawing.Point(12, 13);
+            this.valuec.Name = "valuec";
+            this.valuec.Size = new System.Drawing.Size(21, 15);
+            this.valuec.TabIndex = 1;
+            this.valuec.Text = "50";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(126, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 25);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Import";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // startc
+            // 
+            this.startc.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startc.Location = new System.Drawing.Point(201, 7);
+            this.startc.Name = "startc";
+            this.startc.Size = new System.Drawing.Size(69, 25);
+            this.startc.TabIndex = 4;
+            this.startc.Text = "Start";
+            this.startc.UseVisualStyleBackColor = true;
+            this.startc.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Refresh
+            // 
+            this.Refresh.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Refresh.Location = new System.Drawing.Point(51, 7);
+            this.Refresh.Name = "Refresh";
+            this.Refresh.Size = new System.Drawing.Size(69, 25);
+            this.Refresh.TabIndex = 5;
+            this.Refresh.Text = "Refresh";
+            this.Refresh.UseVisualStyleBackColor = true;
+            this.Refresh.Click += new System.EventHandler(this.Refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(282, 367);
+            this.Controls.Add(this.Refresh);
+            this.Controls.Add(this.startc);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.valuec);
+            this.Controls.Add(this.List);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kakaotalk Status";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.List)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView List;
+        private System.Windows.Forms.Label valuec;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Room;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button startc;
+        private System.Windows.Forms.Button Refresh;
+    }
+}
+
---
+++ KakaotalkStatus/KakaotalkStatus/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.IO;
+
+namespace KakaotalkStatus
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string ipclassname, string ipwindowname);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindowEx(IntPtr hwnd1, IntPtr hwnd2, string ipsz1, string ipsz2);
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hwnd, int wmsg, int wparam, string iparam);
+
+        [DllImport("user32.dll")]
+        public static extern uint PostMessage(IntPtr hwnd, int wmsg, int wparam, int iparam);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindowAsync(IntPtr hwnd, int ncmdshow);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Thread kakaotalkThread;
+        void kakaotalkMain()
+        {
+            int st = 0;
+            List<string> set = new List<string>();
+            List<string> rooms = new List<string>();
+            for (int i = 0; i < List.Rows.Count; i++)
+            {
+                rooms.Add(List.Rows[i].Cells[0].Value.ToString());
+            }
+
+            if (rooms.Count != 0)
+            {
+                for (int i = 0; i < rooms.Count; i++)
+                {
+                    IntPtr room = FindWindow(null, rooms[i]);
+                    if (room != IntPtr.Zero)
+                    {
+                        Thread.Sleep(100);
+                        IntPtr chk = FindWindowEx(room, IntPtr.Zero, "RICHEDIT50W", "");
+                        if (chk != IntPtr.Zero)
+                        {
+                            Thread.Sleep(100);
+                            SendMessage(chk, 0x000c, 0, "Test text"); //0x000c = set text
+                            Thread.Sleep(100);
+                            PostMessage(chk, 0x0100, 0xD, 0x0101); //0x0100 = KeyDown //0xD = 줄바꿈 (Enter 추정) //0x0101 = KeyUp
+
+                            st = 0;
+                        }
+                    }
+                    else
+                    {
+                        st = 1;
+                    }
+                    set.Add(rooms[i] + " " + st);
+                }
+            }
+            List.Rows.Clear();
+            for (int i = 0; i < set.Count; i++)
+            {
+                string value = set[i];
+                var str = value.Split(' ').Last();
+                var room = value.Replace(str, "").Trim();
+                
+                if (str == "0")
+                {
+                    List.Rows.Add(room, "Successed");
+                    List.Rows[i].Cells[1].Style.ForeColor = Color.Green;
+                }
+                else if (str == "1")
+                {
+                    List.Rows.Add(room, "Failed");
+                    List.Rows[i].Cells[1].Style.ForeColor = Color.Red;
+                }
+            }
+        }
+
+        public static string path = null;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "all files (*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                List.Rows.Clear();
+                path = null;
+                path = ofd.FileName;
+                string[] contents = File.ReadAllLines(path);
+                for (int i = 0; i < contents.Length; i++)
+                {
+                    List.Rows.Add(contents[i], "-");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            kakaotalkMain();
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            List.Rows.Clear();
+            string[] contents = File.ReadAllLines(path);
+            for (int i = 0; i < contents.Length; i++)
+            {
+                List.Rows.Add(contents[i], "-");
+            }
+        }
+    }
+}
